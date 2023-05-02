4a4a356249b3587321a36152156c7400f1c1d85f
hecyoon <84567556+hecyoon@users.noreply.github.com>
2023-05-02 18:49:02 +0900

    초기세팅

--- app.js
+++ app.js
@@ -13,27 +13,27 @@ app.use(logger('dev'));
 app.use(express.json());
 
 app.get('/ping', function (req, res, next) {
-    res.status(200).json({ message: 'pong' })
+  res.status(200).json({ message: 'pong' })
 });
 
 const PORT = process.env.PORT;
 
 const start = async () => {
-    try {
-        dataSource
-            .initialize()
-            .then(() => {
-                console.log("DataSource has been initialized!");
-            })
-            .catch((err) => {
-                console.log("DataSource Not Initialize :", err);
-                dataSource.destroy();
-            });
+  try {
+    dataSource
+      .initialize()
+      .then(() => {
+        console.log("DataSource has been initialized!");
+      })
+      .catch((err) => {
+        console.log("DataSource Not Initialize :", err);
+        dataSource.destroy();
+      });
 
-        app.listen(PORT, () => console.log(`🚨 server listening on port ${PORT}`));
-    } catch (err) {
-        console.log(err);
-    }
+    app.listen(PORT, () => console.log(`🚨 server listening on port ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 start();
