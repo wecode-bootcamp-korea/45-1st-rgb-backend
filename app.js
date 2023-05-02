71c9adf2aa12e72449ac5ef3d8b95d121d77fa05
hecyoon <84567556+hecyoon@users.noreply.github.com>
2023-05-02 18:42:03 +0900

    초기세팅

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const logger = require('morgan');
 const express = require('express');
 const cors = require('cors');
+const { dataSource } = require('./src/models/dataSource');
 
 
 const app = express();
@@ -19,6 +20,16 @@ const PORT = process.env.PORT;
 
 const start = async () => {
     try {
+        dataSource
+            .initialize()
+            .then(() => {
+                console.log("DataSource has been initialized!");
+            })
+            .catch((err) => {
+                console.log("DataSource Not Initialize :", err);
+                dataSource.destroy();
+            });
+
         app.listen(PORT, () => console.log(`🚨 server listening on port ${PORT}`));
     } catch (err) {
         console.log(err);
