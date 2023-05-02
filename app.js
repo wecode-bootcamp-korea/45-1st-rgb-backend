df49a58e767c7c1a5d52d7cbf164ffcab584b553
hecyoon <84567556+hecyoon@users.noreply.github.com>
2023-05-02 15:06:18 +0900

    GIT INIT FIXED

---
+++ app.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const logger = require('morgan');
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+
+app.get('/ping', function (req, res, next) {
+    res.status(200).json({ message: 'pong' })
+});
+
+const PORT = process.env.PORT;
+
+const start = async () => {
+    try {
+        dataSource
+            .initialize()
+            .then(() => {
+                console.log("DataSource has been initialized!");
+            })
+            .catch((err) => {
+                console.log("DataSource Not Initialize :", err);
+                dataSource.destroy();
+            });
+        app.listen(PORT, () => console.log(`🚨 server listening on port ${PORT}`));
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+start();
